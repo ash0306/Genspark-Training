a5e8508e7440d0f912ab1592a3463b192a9e4673
unknown <aswathi259@gmail.com>
2024-04-12 17:01:48 +0530

    Added Employee Tracker

---
+++ EmployeeTracker/ApplicationSolution/EmployeeTracker/Models/Employee.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeTracker.Models
+{
+    internal class Employee
+    {
+        double salary;
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public double Salary 
+        {
+            get
+            {
+                return (salary - (salary * 10) / 100);
+            }
+            set
+            {
+                salary = value;
+            }
+        }
+
+        public string Department { get; set; }
+
+        public Employee()
+        {
+            Id = 0;
+            Name = string.Empty;
+            DateOfBirth = DateTime.MinValue;
+            Salary = 0;
+            Department = string.Empty;
+        }
+
+        /// <summary>
+        /// Parameterised Constructor
+        /// </summary>
+        /// <param name="id">Employee ID</param>
+        /// <param name="name">EMployee name as String</param>
+        /// <param name="dateOfBirth">Employee DOB</param>
+        /// <param name="salary">Employee Salary</param>
+        /// <param name="department">Employee Department as string</param>
+        public Employee(int id, string name, DateTime dateOfBirth, double salary, string department)
+        {
+            Id = id;
+            Name = name;
+            DateOfBirth = dateOfBirth;
+            Salary = salary;
+            Department = department;
+        }
+        /// <summary>
+        /// Prints all the Employee Details
+        /// </summary>
+        public void PrintEmployeeDetails()
+        {
+            Console.WriteLine($"Employee ID:\t{Id}");
+            Console.WriteLine($"Employee Name:\t{Name}");
+            Console.WriteLine($"Employee Salary:\t{Salary}");
+            Console.WriteLine($"Employee DOB:\t{DateOfBirth}");
+            Console.WriteLine($"Employee Department:\t{Department}");
+        }
+    }
+}
---
+++ EmployeeTracker/ApplicationSolution/EmployeeTracker/Program.cs
@@ -0,0 +1,47 @@
+﻿using EmployeeTracker.Models;
+namespace EmployeeTracker
+{
+    internal class Program
+    {
+        Employee CreateEmployeeByTakingDetails()
+        {
+            Employee employee = new Employee();
+            Console.WriteLine("Enter Employee ID:");
+            employee.Id = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Employee Name:");
+            employee.Name = Console.ReadLine() ?? "";
+            Console.WriteLine("Enter Employee Date Of Birth:");
+            employee.DateOfBirth = Convert.ToDateTime(Console.ReadLine());
+            Console.WriteLine("Enter Employee Salary:");
+            double salary;
+            while (!double.TryParse(Console.ReadLine(), out salary))
+            {
+                Console.WriteLine("Invalid entry. Please try again.");
+            }
+            employee.Salary = salary;
+            Console.WriteLine("Enter Employee Department:");
+            employee.Department = Console.ReadLine() ?? "";
+
+            return employee;
+        }
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            int numOfEntries;
+            Console.WriteLine("Enter the number of entries you want to add:");
+            numOfEntries = Convert.ToInt32(Console.ReadLine());
+
+            Employee[] employees = new Employee[numOfEntries];
+
+            for (int i = 0; i < numOfEntries; i++)
+            {
+                employees[i] = program.CreateEmployeeByTakingDetails();
+            }
+
+            for (int i = 0; i<numOfEntries; i++)
+            {
+                employees[i].PrintEmployeeDetails();
+            }
+        }
+    }
+}
