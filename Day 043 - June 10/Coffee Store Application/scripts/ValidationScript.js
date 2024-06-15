401f725e350f7784e83d4ee839eb9454f39a4881
unknown <aswathi259@gmail.com>
2024-06-15 12:13:09 +0530

    Added orders for admin

---
+++ Day 043 - June 10/Coffee Store Application/scripts/ValidationScript.js
@@ -0,0 +1,27 @@
+// Fetch all the forms we want to apply custom Bootstrap validation styles to
+var forms = document.querySelectorAll('.needs-validation');
+
+// Loop over them and prevent submission
+Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+
+        form.classList.add('was-validated');
+    }, false);
+});
+
+// Real-time validation
+document.querySelectorAll('.needs-validation .form-control').forEach(function (input) {
+    input.addEventListener('input', function () {
+        if (input.checkValidity()) {
+            input.classList.remove('is-invalid');
+            input.classList.add('is-valid');
+        } else {
+            input.classList.remove('is-valid');
+            input.classList.add('is-invalid');
+        }
+    });
+});
