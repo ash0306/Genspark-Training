bec90ba8df0472800ca55403fcd70e45382600c7
unknown <aswathi259@gmail.com>
2024-06-19 18:47:53 +0530

    Added additional Admin pages and updated API routes

---
+++ Day 043 - June 10/Coffee Store Application/AdminPages/scripts/NavbarScript.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function(){
+    var adminRole = '';
+
+    var token = sessionStorage.getItem('employeeToken');
+    const tokenArray = token.split('.');
+    const tokenPayload = JSON.parse(atob(tokenArray[1]));
+    console.log(tokenPayload);
+    const tokenId = tokenPayload["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier"];
+    adminRole = tokenPayload["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"];
+
+    const navbar = document.getElementById('navBarToggleContent');
+
+    if(adminRole=='Admin'){
+        navbar.innerHTML ='';
+        navbar.innerHTML = `
+        <a href="./Customers.html" class="text-dark text-decoration-none">Customers</a>
+        <a href="./Employees.html" class="text-dark text-decoration-none">Employees</a>
+        <button type="button" href="#" class="btn btn-dark rounded-lg px-3">
+        <a href="./Login.html" class="text-light text-decoration-none">Login</a></button>`;
+    }
+})
--- Day 043 - June 10/Coffee Store Application/AdminPages/scripts/ProductScript.js
+++ Day 043 - June 10/Coffee Store Application/AdminPages/scripts/ProductScript.js
@@ -1,3 +1,5 @@
+var token = sessionStorage.getItem('employeeToken');
+
 document.addEventListener('DOMContentLoaded', function () {
 
     if(sessionStorage.getItem("employeeToken")==null){
@@ -10,17 +12,28 @@ document.addEventListener('DOMContentLoaded', function () {
         return;
     }
 
-    var token = sessionStorage.getItem('employeeToken');
+    getAllProducts();
 
+    const searchInput = document.getElementById('search-input');
+    searchInput.addEventListener('input', async function() {
+        const productName = this.value.trim();
+        if (productName.length === 0) {
+            getAllProducts();
+            return;
+        }
+        searchResults(productName);
+    });
+});
+
+function getAllProducts(){
     fetch('http://localhost:5228/api/product/getAllProducts', {
         method: 'GET',
         headers: {
-            Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjIxMiIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL25hbWUiOiJEb21pbmljIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvZW1haWxhZGRyZXNzIjoiZG9taW5pY0BnbWFpbC5jb20iLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJCYXJpc3RhIiwiZXhwIjoxNzE4ODgwNDUxfQ.L8GXfnw_e2tXyFz7MNSjSCh5jNb-1h6D3aq0DdXSqoY`
+            Authorization: `Bearer ${token}`
         }
     }).then(async (response) => {
-        console.log(response);
+        
         var data = await response.json();
-        console.log(data);
 
         const tableBody = document.getElementsByTagName('tbody')[0];
         tableBody.innerHTML = ''; 
@@ -50,4 +63,42 @@ document.addEventListener('DOMContentLoaded', function () {
     }).catch(error => {
         console.error(error);
     });
-});
+}
+
+function searchResults(productName){
+    fetch('http://localhost:5228/api/product/getAllProducts',{
+        method: 'GET',
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }).then(async(response)=>{
+        const data = await response.json();
+        const tableBody = document.getElementsByTagName('tbody')[0];
+        tableBody.innerHTML = ''; 
+
+        var filteredResults = data.filter(element => element.name.toLowerCase().includes(productName.toLowerCase()));
+
+        filteredResults.forEach(element => {
+            const row = document.createElement('tr');
+
+            let statusColor = '';
+            if (element.status === 'Available') {
+                statusColor = 'text-success';
+            } else if (element.status === 'Unavailable') {
+                statusColor = 'text-danger';
+            } else {
+                statusColor = 'text-warning';
+            }
+            
+            row.innerHTML = `
+                <td>${element.name}</td>
+                <td><img src="${element.image}"></td>
+                <td>${element.description}</td>
+                <td>${element.category}</td>
+                <td>${element.price}</td>
+                <td class="${statusColor}">${element.status}</td>
+                <td>${element.stock}</td>`;
+            tableBody.appendChild(row);
+        });
+    }).catch(error => console.log(error));
+}
