d9e339b6c7abc287d4f63fe01c0fedd42efe5d51
unknown <aswathi259@gmail.com>
2024-06-23 12:12:10 +0530

    Added route to update order status

--- Day 043 - June 10/Coffee Store Application/Admin/assets/scripts/OrderScript.js
+++ Day 043 - June 10/Coffee Store Application/Admin/assets/scripts/OrderScript.js
@@ -1,16 +1,17 @@
-document.addEventListener('DOMContentLoaded', function(){
+var token = sessionStorage.getItem('employeeToken');
 
-    var token = sessionStorage.getItem('employeeToken');
+document.addEventListener('DOMContentLoaded', function() {
+    displayOrders();
+});
 
+function displayOrders() {
     fetch("http://localhost:5228/api/orders/pendingOrders", {
         method: 'GET',
         headers: {
             Authorization: `Bearer ${token}`
         }
     }).then(async (response) => {
-
         const data = await response.json();
-        console.log(data);
 
         var placedDiv = document.getElementById('placedOrder');
         placedDiv.innerHTML = '';
@@ -18,7 +19,7 @@ document.addEventListener('DOMContentLoaded', function(){
         preparingDiv.innerHTML = '';
         var readyDiv = document.getElementById('readyOrder');
         readyDiv.innerHTML = '';
-        
+
         data.forEach(element => {
             var orderList = document.createElement('ul');
 
@@ -28,10 +29,11 @@ document.addEventListener('DOMContentLoaded', function(){
                 orderList.appendChild(listItem);
             });
 
-            if(element.status === 'Placed'){
-                const card = document.createElement('div');
-                card.classList.add('card', 'col-md-3');
-                card.id = 'placed-order'
+            const card = document.createElement('div');
+            card.classList.add('card', 'col-md-3');
+
+            if (element.status === 'Placed') {
+                card.id = 'placed-order';
                 card.innerHTML = `
                     <div class="card-body h-auto">
                         <h5 class="card-title">Order #${element.id}</h5>
@@ -40,15 +42,12 @@ document.addEventListener('DOMContentLoaded', function(){
                                 ${orderList.innerHTML}
                             </ul>
                         </p>
-                        <a href="#" class="btn btn-dark">Move to preparing</a>
+                        <a href="#" class="btn btn-dark" onclick="updateOrderStatus(${element.id}, 'Preparing')">Move to preparing</a>
                     </div>
                 `;
                 placedDiv.appendChild(card);
-            }
-            else if(element.status === 'Preparing'){
-                const card = document.createElement('div');
-                card.classList.add('card', 'col-md-3');
-                card.id = 'preparing-order'
+            } else if (element.status === 'Preparing') {
+                card.id = 'preparing-order';
                 card.innerHTML = `
                     <div class="card-body h-auto">
                         <h5 class="card-title">Order #${element.id}</h5>
@@ -57,15 +56,12 @@ document.addEventListener('DOMContentLoaded', function(){
                                 ${orderList.innerHTML}
                             </ul>
                         </p>
-                        <a href="#" class="btn btn-dark">Move to ready</a>
+                        <a href="#" class="btn btn-dark" onclick="updateOrderStatus(${element.id}, 'Ready')">Move to ready</a>
                     </div>
                 `;
                 preparingDiv.appendChild(card);
-            }
-            else if(element.status === 'Ready'){
-                const card = document.createElement('div');
-                card.classList.add('card', 'col-md-3');
-                card.id ='ready-order'
+            } else if (element.status === 'Ready') {
+                card.id = 'ready-order';
                 card.innerHTML = `
                     <div class="card-body h-auto">
                         <h5 class="card-title">Order #${element.id}</h5>
@@ -74,13 +70,36 @@ document.addEventListener('DOMContentLoaded', function(){
                                 ${orderList.innerHTML}
                             </ul>
                         </p>
-                        <a href="#" class="btn btn-dark">Move to completed</a>
+                        <a href="#" class="btn btn-dark" onclick="updateOrderStatus(${element.id}, 'Completed')">Move to completed</a>
                     </div>
                 `;
                 readyDiv.appendChild(card);
             }
         });
-    }).catch(error =>{
+    }).catch(error => {
         console.error(error);
-    })
-});
+    });
+}
+
+function updateOrderStatus(orderId, orderStatus) {
+    fetch("http://localhost:5228/api/orders/updateOrderStatus", {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify({
+            orderId: parseInt(orderId),
+            status: orderStatus
+        })
+    }).then(async (response) => {
+        var data = await response.json();
+        console.log(data);
+
+        if (response.status === 200) {
+            displayOrders();
+        }
+    }).catch(error => {
+        console.error(error);
+    });
+}
