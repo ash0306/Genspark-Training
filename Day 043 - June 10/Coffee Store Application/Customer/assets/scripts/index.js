e48e5cdbb78151dde37bc698d147492dda205006
unknown <aswathi259@gmail.com>
2024-06-26 10:09:28 +0530

    Updated admin order design and other minor styles and design

--- Day 043 - June 10/Coffee Store Application/Customer/assets/scripts/index.js
+++ Day 043 - June 10/Coffee Store Application/Customer/assets/scripts/index.js
@@ -1,1 +1,8 @@
 AOS.init({ duration: 1500 });
+
+var typed = new Typed("#typed-element",{
+    strings: ["Experience bliss in every sip!!","Experience flavour in every blend!!","Experience joy in every bite!!"],
+    typeSpeed: 50,
+    backSpeed: 25,
+    cursorChar: ""
+})
