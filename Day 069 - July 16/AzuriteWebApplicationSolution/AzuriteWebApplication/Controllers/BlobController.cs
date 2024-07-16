d65eb605244cd83f9d7bcaf4b334a44c980f7ba8
unknown <aswathi259@gmail.com>
2024-07-16 17:27:21 +0530

    Day 69 works added

---
+++ Day 069 - July 16/AzuriteWebApplicationSolution/AzuriteWebApplication/Controllers/BlobController.cs
@@ -0,0 +1,41 @@
+﻿using AzuriteWebApplication.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace AzuriteWebApplication.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BlobController : ControllerBase
+    {
+        private readonly BlobService _blobService;
+
+        public BlobController(BlobService blobService)
+        {
+            _blobService = blobService;
+        }
+
+        [HttpPost("upload")]
+        public async Task<IActionResult> UploadBlob([FromQuery] string containerName, [FromQuery] string blobName, IFormFile file)
+        {
+            using var stream = file.OpenReadStream();
+            await _blobService.UploadBlobAsync(containerName, blobName, stream);
+            return Ok();
+        }
+
+        [HttpGet("download")]
+        public async Task<IActionResult> DownloadBlob([FromQuery] string containerName, [FromQuery] string blobName)
+        {
+            var stream = await _blobService.GetBlobAsync(containerName, blobName);
+            return File(stream, "application/octet-stream");
+        }
+
+        [HttpDelete("delete")]
+        public async Task<IActionResult> DeleteBlob([FromQuery] string containerName, [FromQuery] string blobName)
+        {
+            await _blobService.DeleteBlobAsync(containerName, blobName);
+            return NoContent();
+        }
+    }
+}
---
+++ Day 069 - July 16/README.md
@@ -0,0 +1,13 @@
+# Day 69
+
+## Topics Covered
+
+- SQL Server in Azure
+- Azure Storage Accounts
+- Azure CLI
+
+## Work Done
+
+- Tried out blob storage in Azure
+
+- Tried out Azurite emulator. The work done on this can be found [here](./AzuriteWebApplicationSolution/)
